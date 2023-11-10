00013a837620eac7bc343ef2dc7521ac199380f5
iliyaahmadi <iliyahmdi@yahoo.com>
2023-11-10 22:40:23 +0330

    feat[error] : handle unhandledRejection errors

--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ const app = express();
 const db = require('./models/index');
 const CustomError = require('./utils/customError');
 const globalErrorHandler = require('./middlewares/error-handler');
+const fs = require('fs');
 global.__basedir = __dirname;
 
 // --temp for initializing server / reseting DB--
@@ -35,6 +36,25 @@ app.use(globalErrorHandler);
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`OutRight Api running on port ${port}`);
+});
+
+process.on('unhandledRejection', (err) => {
+  const utc = Date.now();
+  const date = new Date(utc).toString();
+  const errObject = {
+    time: date,
+    error_name: err.name,
+    error_message: err.message,
+    error: err,
+  };
+  fs.appendFileSync(
+    `${__dirname}/logs/unhandledRejection.txt`,
+    JSON.stringify(errObject, null, 2),
+    'UTF-8'
+  );
+  server.close(() => {
+    process.exit(1);
+  });
 });
