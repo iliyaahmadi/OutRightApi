a3c4a071f23deabb5e4a1a79acb0f1a45df908ce
iliyaahmadi <iliyahmdi@yahoo.com>
2023-11-11 18:22:35 +0330

    feat[blog] : add asynchandler to blog funcs

--- src/controllers/blog-controller.js
+++ src/controllers/blog-controller.js
@@ -3,7 +3,7 @@ const Section = require('../models').section;
 const SectionImg = require('../models').sectionImg;
 const asyncErrorHandler = require('../utils/asyncErrorHandler');
 
-const findAll = async (req, res , next) => {
+const findAll = asyncErrorHandler(async (req, res, next) => {
   const blogs = await Blog.findAll({
     attributes: [
       'id',
@@ -17,9 +17,9 @@ const findAll = async (req, res , next) => {
     include: [{ model: Section, include: [SectionImg] }],
   });
   res.status(200).json(blogs);
-};
+});
 
-const findById = async (req, res , next) => {
+const findById = asyncErrorHandler(async (req, res, next) => {
   await Blog.findOne({
     where: {
       id: req.params.id,
@@ -37,13 +37,9 @@ const findById = async (req, res , next) => {
     .then((blog) => {
       return res.status(200).json(blog);
     })
-    .catch((err) => {
-      console.log(err);
-      return res.status(400).json({ message: err });
-    });
-};
+});
 
-const create = async (req, res , next) => {
+const create = asyncErrorHandler(async (req, res, next) => {
   await Blog.create({
     title: req.body.title,
     slug: req.body.slug,
@@ -53,9 +49,9 @@ const create = async (req, res , next) => {
   res.status(201).json({
     message: `پست شما با موفقیت ساخته شد`,
   });
-};
+});
 
-const edit = async (req, res , next) => {
+const edit = asyncErrorHandler(async (req, res, next) => {
   const target = await Blog.findOne({
     where: {
       id: req.params.id,
@@ -77,9 +73,9 @@ const edit = async (req, res , next) => {
   res.status(200).json({
     message: ` پست با مشخصات داده شده تغییر کرد`,
   });
-};
+});
 
-const remove = async (req, res , next) => {
+const remove = asyncErrorHandler(async (req, res, next) => {
   const id = req.params.id;
   Blog.destroy({
     where: {
@@ -92,12 +88,7 @@ const remove = async (req, res , next) => {
         .json({ message: `پست مورد نظر با این آیدی پاک شد ${id}` });
       return;
     })
-    .catch((err) => {
-      res.status(400).json({ message: err });
-      console.log(err);
-      return;
-    });
-};
+});
 
 module.exports = {
   findAll,
