b972aca9c482a9e251e1c6d8d1b8074930f1032d
iliyaahmadi <iliyahmdi@yahoo.com>
2023-11-19 22:30:18 +0330

    feat [review] : add like handler

--- src/controllers/Review-controller.js
+++ src/controllers/Review-controller.js
@@ -1,5 +1,5 @@
-const Product = require('../models').product;
 const Review = require('../models').review;
+const ReviewLikes = require('../models').review_likes;
 const asyncErrorHandler = require('../utils/asyncErrorHandler');
 
 const findAll = asyncErrorHandler(async (req, res, next) => {
@@ -43,12 +43,47 @@ const removeByAdmin = asyncErrorHandler(async (req, res, next) => {
   return res.status(200).json({ message: 'پیام حذف شد' });
 });
 
-
+const like = asyncErrorHandler(async (req, res) => {
+  let reviewId = req.params.id;
+  let userId = req.userId;
+  //we check if user liked it before
+  const exists = await ReviewLikes.findOne({
+    where: {
+      userId: userId,
+      reviewId: reviewId,
+    },
+  });
+  if (exists) {
+    //if already liked
+    await ReviewLikes.destroy({
+      where: {
+        userId: userId,
+        reviewId: reviewId,
+      }, //track
+    }).then(async () => {
+      await Review.decrement('likes', {
+        where: { id: reviewId },
+      });
+    });
+    return res.status(200).json({ message: `لایک برداشته شد` });
+  } else {
+    //if was not liked before
+    await ReviewLikes.create({
+      userId: userId,
+      reviewId: reviewId,
+    }).then(async () => {
+      await Review.increment('likes', {
+        where: { id: reviewId },
+      });
+    });
+    return res.status(200).json({ message: `لایک شد` });
+  }
+});
 module.exports = {
   findAll,
-  create,
+  // create,
   remove,
   removeByAdmin,
   like,
-  dislike,
+  // dislike,
 };
